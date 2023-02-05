a93313d4fe6eebff0419be7260b1f8afad07be2b
Gustavo Araújo Morais <g170603@dac.unicamp.br>
2023-02-05 12:11:10 -0300

    Tutorial

--- scenes/scripts/Test.gd
+++ scenes/scripts/Test.gd
@@ -5,8 +5,23 @@ const MIN_SPAWN_TIME = 0.5
 const MAX_SPAWN_TIME = 7
 
 
+export (bool) var tutorial = false
+
+
 onready var gui = $GUI
 
 
+func _ready():
+	if tutorial:
+		$Viewport/Nivel/ResourceLiquid.tutorial = tutorial
+		gui.tutorial = tutorial
+
+
 func get_enemy_spawn_time():
+	if tutorial:
+		return 999999999999
 	return MAX_SPAWN_TIME - (MAX_SPAWN_TIME - MIN_SPAWN_TIME) * gui.resource4Value / gui.MAX_FINAL_RESOURCE_VALUE
+
+
+func to_menu():
+	get_tree().change_scene("res://scenes/Menu.tscn")
